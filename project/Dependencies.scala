bcc7ba577db7257b9e3de28ca37f8c9d870cd208
ellie-l-newell <113107044+ellie-l-newell@users.noreply.github.com>
2026-02-16 14:37:08 +0000

    Upgrade sbt and Scala versions and update play-json dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,10 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "performance-test-runner"  % "6.2.0",
-    "org.playframework" %% "play-json"                % "3.0.5"
+    "org.playframework" %% "play-json"                % "3.0.6"
   ).map(_ % Test)
 
 }
